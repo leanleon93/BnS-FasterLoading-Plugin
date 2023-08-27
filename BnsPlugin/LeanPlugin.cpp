811eb2916f69ac4bf3711ab55385f7bf492563df
Leon W <31262614+leanleon93@users.noreply.github.com>
2023-08-27 03:33:20 +0200

    Add: fix for storage tradeable icons disappearing

--- BnsPlugin/LeanPlugin.cpp
+++ BnsPlugin/LeanPlugin.cpp
@@ -1,13 +1,41 @@
-#pragma once
 #include <thread>
 #include <pe/module.h>
-#include "detours.h";
+#include "detours.h"
 #include "LeanPlugin.h"
 #include <iostream>
-#include "searchers.h";
-#include "Hooks.h";
-#include "xorstr.hpp";
+#include "searchers.h"
+#include "Hooks.h"
+#include "xorstr.hpp"
 #include "PluginConfig.h"
+
+struct DETOUR_ALIGN
+{
+	BYTE    obTarget : 3;
+	BYTE    obTrampoline : 5;
+};
+
+struct DETOUR_INFO
+{
+	// An X64 instuction can be 15 bytes long.
+	// In practice 11 seems to be the limit.
+	BYTE            rbCode[30];     // target code + jmp to pbRemain.
+	BYTE            cbCode;         // size of moved target code.
+	BYTE            cbCodeBreak;    // padding to make debugging easier.
+	BYTE            rbRestore[30];  // original target code.
+	BYTE            cbRestore;      // size of original target code.
+	BYTE            cbRestoreBreak; // padding to make debugging easier.
+	DETOUR_ALIGN    rAlign[8];      // instruction alignment array.
+	PBYTE           pbRemain;       // first instruction after moved code. [free list]
+	PBYTE           pbDetour;       // first instruction of detour function.
+	BYTE            rbCodeIn[8];    // jmp [pbDetour]
+};
+
+extern "C"
+{
+	void hkStorageFix();
+	LPVOID lpRemain;
+	int64_t ogAddress;
+}
 
 gsl::span<uint8_t> data;
 pe::module* module;
@@ -109,8 +137,24 @@ void WINAPI InitDetours() {
 		DetourAttach(&(PVOID&)BNSClient_GetWorld, &hkBNSClient_GetWorld);
 	}
 
+	PDETOUR_TRAMPOLINE lpTrampolineData = {}; //Datas to store information about hooking
+	auto sStorageFix = std::search(data.begin(), data.end(), pattern_searcher(xorstr_("4C 8B 0E 48 8B CE 44 0F B6 80 32 03 00 00 8B 97 D4 4F 00 00 41 FF 91 88 01 00 00 8B 97 D4 4F 00 00 48 8B CF")));
+	if (sStorageFix != data.end()) {
+		uintptr_t aStorageFix = (uintptr_t)((uintptr_t)&sStorageFix[0] - 0x0E);
+		ogAddress = (*(int32_t*)(aStorageFix + 1)) + aStorageFix + 5;
+#ifdef _DEBUG
+		printf("Address of aStorageFix is %p\n", (void*)aStorageFix);
+		printf("Address of ogAddress in %p\n", (void*)ogAddress);
+		std::cout << std::endl;
+#endif // _DEBUG
+		DetourAttachEx(&(PVOID&)aStorageFix, (PVOID)&hkStorageFix, &lpTrampolineData, nullptr, nullptr); //Use DetourAtttachEx to retrieve information about the hook
+	}
 
 	DetourTransactionCommit();
+
+	const auto lpDetourInfo = (DETOUR_INFO*)lpTrampolineData;
+	if (lpDetourInfo != nullptr)
+		lpRemain = lpDetourInfo->pbRemain; //Retrieve the address to jump back the original function
 }
 
 
